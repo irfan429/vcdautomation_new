355223645af2114278217f3cee1890762ba3115f
aravedu <araveduirfanvali9@gmail.com>
2021-07-27 11:47:49 +0530

    changed testupdates

--- testsuites/Test_Updates.py
+++ testsuites/Test_Updates.py
@@ -4,8 +4,10 @@ import allure
 import pytest
 from pytest_testrail.plugin import pytestrail
 
-@pytest.mark.Regression
-@pytestrail.case('C7246')
+
+# @pytest.mark.Regression
+# @pytestrail.case('C7246')
+
 @allure.title("C7246: Verify that you can access VCD")
 def test_case_C7246(test_case_template):
     new_case = test_case_template
@@ -14,4 +16,4 @@ def test_case_C7246(test_case_template):
     # new_case.click_upload_new_image_button()
     # new_case.pyauto_uploadFile("\\data\\newImageFile\\VIEW_NtWC_Pr10_MCU_2.4.5.tgz", 'enter')
 
-    time.sleep(5)
+    # time.sleep(5)
